bb5b87dcbdcb7e99d1fbecca5a2558c75e469a56
beneon <lucidstudio@gmail.com>
2017-01-08 23:35:44 +0800

    今天只能写到这里了
    到最后面提取关键词的时候还是有些问题啊

--- extractDOI.js
+++ extractDOI.js
@@ -1,14 +1,50 @@
 var fs = require('fs')
+var path = require('path')
 var data = fs.readFile('ref.txt','utf8',function(err,data){
   var processed = doiPreProcess(data)
   processed = doiProcessSecondary(processed)
-  console.log(processed.split(/\r?\n/).length)
-  console.log(processed)
+  processed = htmlGen(processed)
+  fs.writeFile(path.join(__dirname,outputFilename),processed,(e)=>{
+    if(e)throw e
+    console.log('saved!')
+  })
 })
+const outputFilename = "search.html"
 const doiReg = /(doi:10\.\d{4}(\.\d+)?\/.*[\w\.\-\/].*[\d\w])\r?\n/g
 const doiWild = /(doi:.*)\r?\n/g
 const endingPatternStrict = /.*(\d:\d*.\d+)\r?\n|.*(doi:.*)\r?\n/g
-
+const RESTFULPrefix = "http://xueshu.baidu.com/s?wd="
+const keyword1 = /doi:(.*)/
+const keyword2 = /\. ((?:\w* )*\d*:\d*–\d*)/
+function htmlGen(data){
+  var entries = data.split(/\r?\n/)
+  var resultTemplate = '<!DOCTYPE html> <html> <head> <meta charset="utf-8"> <title></title> </head> <body>{content}</body> </html>'
+  var hrefTemplate = '<a target="_blank" href="{link}">网上查询</a>'
+  var result = ""
+  console.log(entries.length)
+  entries.forEach(function(e){
+    var link = keywordExtract(e)
+    var aTxt = hrefTemplate.replace("{link}",keywordExtract(e))
+    console.log(link)
+    result += '<p>'+e+link?('<br>'+aTxt):''+'</p>'
+    // console.log(result)
+  })
+  resultTemplate =  resultTemplate.replace("{content}",result)
+  return resultTemplate
+}
+function keywordExtract(str){
+  var result = keyword1.exec(str)
+  if(result){
+    return result[1]
+  }else{
+    var result2 = keyword2.exec(str)
+    if(result2){
+      return result2[1]
+    }else{
+      return null
+    }
+  }
+}
 function getRidOfTheNewLine(txt,matcher){
   var replacer = matcher.replace(/\r?\n/,"")
   return txt.replace(matcher,replacer)
