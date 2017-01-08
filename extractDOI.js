47b1bced0a7f5ba482cd10f5cc20c53ae4a664da
beneon <lucidstudio@gmail.com>
2017-01-08 17:46:00 +0800

    Doi identification
    (doi:10\.\d{4}(\.\d+)?\/.*[\.\-\/\d].*)\r?\n
    这个是比较合适的规则
    但是有的时候会因为pdf的异常换行导致无法匹配，所以就用到了下面的这个
    (doi:*)\r*\n
    这个就可以全部匹配了
    (在这之前首先把doi:这四个字符里面所有可能出现的换行符都给去掉）
    这之后就把所有的doi标识符里的异常换行都给删掉。然后就可以提取doi了

--- extractDOI.js
+++ extractDOI.js
@@ -1,18 +1,38 @@
 var fs = require('fs')
 var data = fs.readFile('ref.txt','utf8',function(err,data){
-  var lines = data.split('\r?\n')
-  var matcher = /^([A-Z].*\(\d{4}\))/
-  var result = "<p>"
-  lines.forEach(function(e,i,a){
-    if(e.match(matcher) != null){
-      result += '</p><p>'+e
-    }else{
-      result += e
-    }
-  })
-console.log(result)
-  var doiExt = /doi:(.*)/g
-
-  var htmlResult = result.replace(doiExt,'<a href="https://www.ncbi.nlm.nih.gov/pubmed/?term=$1" target="_blank">$1</a></p>>')
-  fs.writeFile('search.html',htmlResult)
+  var processed = doiIden(data)
+  console.log(processed)
 })
+
+function lineMerger(lines){
+  const type1Reg = /^[A-Z][\w\s]/
+  const type2Reg =
+  lines.forEach(function(e){
+
+  })
+}
+function getRidOfTheNewLine(txt,matcher){
+  var replacer = matcher.replace(/\r?\n/,"")
+  return txt.replace(matcher,replacer)
+}
+function doiIden(txt){
+  var result = txt
+  result.replace(/(d)\s?\r?\n(oi:)/,'$1$2')
+  result.replace(/(do)\s?\r?\n(i:)/,'$1$2')
+  result.replace(/(doi)\s?\r?\n(:)/,'$1$2')
+
+  const doiReg = /(doi:10\.\d{4}(\.\d+)?\/.*[\.\-\/\d].*)\r?\n/g
+  const doiWild = /(doi:.*)\r?\n/g
+  var doiStrict = result.match(doiReg)
+  var doiLoose = result.match(doiWild)
+  var filtered = doiLoose.filter(function(e){
+    var exist = doiStrict.find(function(val){
+      return val == e
+    })
+    return exist == undefined
+  })
+  filtered.forEach(function(e){
+    result = getRidOfTheNewLine(result,e)
+  })
+  return result
+}
