2a9cb67b85e4b936963ac80ab373e8cd6cded153
beneon <lucidstudio@gmail.com>
2017-01-08 22:12:17 +0800

    分行部分基本完成
    中间改过几个bug，现在基本正常运行
    接下来是开始做文献搜索链接的制备

--- extractDOI.js
+++ extractDOI.js
@@ -1,32 +1,41 @@
 var fs = require('fs')
 var data = fs.readFile('ref.txt','utf8',function(err,data){
-  var processed = doiIden(data)
+  var processed = doiPreProcess(data)
+  processed = doiProcessSecondary(processed)
+  console.log(processed.split(/\r?\n/).length)
   console.log(processed)
 })
+const doiReg = /(doi:10\.\d{4}(\.\d+)?\/.*[\w\.\-\/].*[\d\w])\r?\n/g
+const doiWild = /(doi:.*)\r?\n/g
+const endingPatternStrict = /.*(\d:\d*.\d+)\r?\n|.*(doi:.*)\r?\n/g
 
-function lineMerger(lines){
-  const type1Reg = /^[A-Z][\w\s]/
-  const type2Reg =
-  lines.forEach(function(e){
-
-  })
-}
 function getRidOfTheNewLine(txt,matcher){
   var replacer = matcher.replace(/\r?\n/,"")
   return txt.replace(matcher,replacer)
 }
-function doiIden(txt){
-  var result = txt
-  result.replace(/(d)\s?\r?\n(oi:)/,'$1$2')
-  result.replace(/(do)\s?\r?\n(i:)/,'$1$2')
-  result.replace(/(doi)\s?\r?\n(:)/,'$1$2')
 
-  const doiReg = /(doi:10\.\d{4}(\.\d+)?\/.*[\.\-\/\d].*)\r?\n/g
-  const doiWild = /(doi:.*)\r?\n/g
-  var doiStrict = result.match(doiReg)
-  var doiLoose = result.match(doiWild)
-  var filtered = doiLoose.filter(function(e){
-    var exist = doiStrict.find(function(val){
+function doiPreProcess(data){
+  var result = data
+  var r2 = result
+  result = result.replace(/(d)\s?\r?\n(oi:)/,'$1$2').replace(/(do)\s?\r?\n(i:)/,'$1$2').replace(/(doi)\s?\r?\n(:)/,'$1$2')
+  result = result.replace(/(doi:[^\s]*)\s([A-Z])/g,'$1\n$2')
+  result = result.replace(/(doi:[^\s]*) ([^\s])/g,'$1$2')
+  result = LooseAndStrict(result,doiWild,doiReg)
+  return result
+}
+function doiProcessSecondary(data){
+  var result = data
+  result = LooseAndStrict(result,/.*\r?\n/g,endingPatternStrict)
+  return result
+}
+
+function LooseAndStrict(data,regLoose,regStrict){
+  var result = data
+  var loose = result.match(regLoose)
+  var strict = result.match(regStrict)
+  var filtered = loose.filter(function(e){
+    var exist = strict.find(function(val){
+      // console.log("%s,%s",e,val)
       return val == e
     })
     return exist == undefined
