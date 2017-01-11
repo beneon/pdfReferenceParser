c2a5afe4d14adb4d12907144e4a0f66eaa40e1f6
beneon <lucidstudio@gmail.com>
2017-01-11 15:56:53 +0800

    基本ok
    接下来要让输出结果和原来的article数据相对接

--- contentExtract.js
+++ contentExtract.js
@@ -3,6 +3,7 @@ var path = require('path')
 var src = "目次utf8.txt"
 var encoding = "utf-8"
 var outputFilename = "目录.json"
+var phraseData
 // endingPhrase.json > parsing function
 
 // sectionName: > title 数字-数字
@@ -14,7 +15,7 @@ var data = fs.readFile(src,encoding,function(err,data){
 function sectionCut(data){
   const src = "endingPhrase.json"
   var phrases = fs.readFile(src,'utf-8',function(err,phraseLog){
-    var phraseData = JSON.parse(phraseLog)
+    phraseData = JSON.parse(phraseLog)
     var section = phraseData.section
     var content = []
     var sectionIndex = section.map(function(e){
@@ -34,7 +35,6 @@ function sectionCut(data){
         return 0
       }
     })
-    console.log(sectionIndex)
     sectionIndex.forEach(function(e,i,a){
       if(a[i+1]){
         content.push({ttl:e.ttl,content:data.substring(e.ind+e.ttl.length,a[i+1].ind).trim()})
@@ -42,6 +42,30 @@ function sectionCut(data){
         content.push({ttl:e.ttl,content:data.substring(e.ind+e.ttl.length).trim()})
       }
     })
-    console.log(content)
+    content = content.map(entriesCut)
   })
+}
+function entriesCut(section){
+  function entryParse(data,lineDelim){
+    var dataArr = data.split(lineDelim)
+    dataArr = dataArr.filter(function(e){
+      return e!=""
+    })
+    var parseDelim = phraseData.eP
+    var matcher = new RegExp(parseDelim.join('|'),'g')
+    dataArr = dataArr.map(function(e){
+      var finding = e.match(matcher).pop()
+      var delimmer = new RegExp(finding)
+      var delimIndex = delimmer.exec(e).index+finding.length
+      var title = e.substring(0,delimIndex).trim().replace(/\s/g,"")
+      var author = e.substring(delimIndex).trim()
+      return {"title":title,"author":author}
+    })
+    console.log(dataArr)
+  }
+  if(section.ttl=="手术录像" || section.ttl=="视频讲座"){
+    entryParse(section.content,/\n/)
+  }else{
+    entryParse(section.content,/\d{1,3}\-\d{1,3}/)
+  }
 }
